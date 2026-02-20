2e75c5adacbfdab11e3c3021d345b08a13a35aeb
pranavdessaii <kishrgdesai@gmail.com>
2026-02-20 13:07:41 +0530

    final

---
+++ calculator.py
@@ -0,0 +1,70 @@
+from itertools import accumulate
+
+logo = r"""
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+print(logo)
+
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+" : add,
+    "-" : subtract,
+    "*" : multiply,
+    "/" : divide,
+}
+
+def calculator():
+    n1 = float(input("Enter first number: "))
+    accumulate =  True
+
+    while accumulate :
+        for key in operations:
+            print(key)
+
+        oper = input("Enter an operation: ")
+        n2 = float(input("Enter second number: "))
+
+        if oper in operations:
+            result = operations[oper](n1, n2)
+            print(f"{n1} {oper} {n2} = {result}")
+        else:
+            print("Invalid operation")
+            continue
+
+        wanna_continue = input(
+            f"Type 'y' to continue with {result}, or 'n' to start new: "
+        )
+
+        if wanna_continue == "y":
+            n1 = result
+        else:
+            accumulate = False
+            print("\n " * 20)
+            calculator()
+
+
+calculator()
