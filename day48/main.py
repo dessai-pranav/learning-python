75b784936dfd60606842519a1ec7e711be6ae44e
pranavdessaii <kishrgdesai@gmail.com>
2026-04-08 19:50:50 +0530

    dictionary

--- day48/main.py
+++ day48/main.py
@@ -5,7 +5,7 @@ service = Service("/usr/bin/chromedriver")
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(service=service,options=chrome_options)
-driver.get("https://www.python.org/")
+driver.get("https://www.python.org")
 
 # price_Dollar = driver.find_element(By.CLASS_NAME,value="a-price-whole")
 # price_cents = driver.find_element(By.CLASS_NAME,value="a-price-fraction")
@@ -14,4 +14,13 @@ driver.get("https://www.python.org/")
 # print(search_bar.get_attribute("placeholder"))
 # button =driver.find_element(By.NAME,value="submit")
 # print(button.size)
-driver.quit()
+
+event_times = driver.find_elements(By.CSS_SELECTOR, ".event-widget time")
+event_names = driver.find_elements(By.CSS_SELECTOR,value=".event-widget li  a")
+events = {}
+for n in range(len(event_times)):
+    events[n] ={
+        "time": event_times[n].text,
+        "name": event_names[n].text,
+    }
+print(events)
