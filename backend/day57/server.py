14b9e4f5fad8a53c587dc9102bbf127d553c7183
pranavdessaii <kishrgdesai@gmail.com>
2026-04-19 11:01:07 +0530

    for loop in jinja

--- backend/day57/server.py
+++ backend/day57/server.py
@@ -20,5 +20,12 @@ def guess(name):
         response2 = requests.get(f"https://api.agify.io?name={name}").json()
         return render_template("index.html",name=name,gender=gender,age=response2["age"])
 
+@app.route("/blog")
+def blog():
+    blog_url ="https://api.npoint.io/c790b4d5cab58020d391"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template("blog.html",posts=all_posts)
+
 if __name__ == "__main__":
     app.run(debug=True)
