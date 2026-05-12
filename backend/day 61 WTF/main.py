ea3647b0ad0c266edcbc6d05273122142ec95385
pranavdessaii <kishrgdesai@gmail.com>
2026-05-12 22:51:58 +0530

    validations

--- backend/day 61 WTF/main.py
+++ backend/day 61 WTF/main.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import DataRequired, Email, length
 from wtforms.fields import StringField
 import secrets
 app = Flask(__name__)
 app.config['SECRET_KEY'] = secrets.token_hex(16)
 
 class MyForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(message='Email is required'), Email(message='Email is invalid')])
+    password = PasswordField('Password', validators=[DataRequired(),length(min=8,message='Password must be at least 8 characters')])
     submit = SubmitField('Submit')
 
 @app.route("/")
