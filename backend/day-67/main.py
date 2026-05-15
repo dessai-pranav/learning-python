6ab123471f7a0c3570db34d50505096c9ada99cb
pranavdessaii <kishrgdesai@gmail.com>
2026-05-15 10:49:03 +0530

    delete

--- backend/day-67/main.py
+++ backend/day-67/main.py
@@ -110,6 +110,12 @@ def edit_post(post_id):
 
 
 # TODO: delete_post() to remove a blog post from the database
+@app.route('/delete_post/<int:post_id>')
+def delete_post(post_id):
+    post = db.get_or_404(BlogPost, post_id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(url_for('get_all_posts'))
 
 # Below is the code from previous lessons. No changes needed.
 @app.route("/about")
