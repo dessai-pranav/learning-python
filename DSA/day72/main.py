01e976a0c8b46930f12c7876d2f52146d5b80a88
pranavdessaii <kishrgdesai@gmail.com>
2026-05-16 16:50:32 +0530

    head max min

--- DSA/day72/main.py
+++ DSA/day72/main.py
@@ -1,3 +1,20 @@
 import pandas as pd
+
 df = pd.read_csv("salaries_by_college_major.csv")
-df.head()
+
+clean_df = df.dropna()
+
+spread_col = (
+    clean_df["Mid-Career 90th Percentile Salary"]
+    - clean_df["Mid-Career 10th Percentile Salary"]
+)
+
+clean_df.insert(1, "spread", spread_col)
+
+low_risk = clean_df.sort_values("spread")
+
+highest_potential = clean_df.sort_values(['Mid-Career 90th Percentile Salary'],ascending = False)
+highest_potential[['Undergraduate Major','Mid-Career 90th Percentile Salary']].head()
+pd.options.display.float_format = '{:,.2f}'.format
+clean_df.groupby('Group').mean(numeric_only=True)
+
